773ea383a57639942ca1bca2d26d2a7bb841c3bc
小澤 孝幸 <dev-taka@mail.wbs.ne.jp>
2019-08-21 21:52:38 +0900

    新規登録

---
+++ Ebi/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Ebi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPrint = new System.Windows.Forms.ToolStripMenuItem();
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.menuImportExcel = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.mainStatusBar = new System.Windows.Forms.StatusStrip();
+            this.xlsOpenDlg = new System.Windows.Forms.OpenFileDialog();
+            this.btnAllSelect = new System.Windows.Forms.Button();
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.mainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.rightPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuPrint});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(1020, 24);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuImportExcel,
+            this.toolStripMenuItem1,
+            this.menuExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(67, 20);
+            this.menuFile.Text = "ファイル(&F)";
+            // 
+            // menuPrint
+            // 
+            this.menuPrint.Name = "menuPrint";
+            this.menuPrint.Size = new System.Drawing.Size(58, 20);
+            this.menuPrint.Text = "印刷(&P)";
+            this.menuPrint.Click += new System.EventHandler(this.MenuPrint_Click);
+            // 
+            // previewBox
+            // 
+            this.previewBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.previewBox.Location = new System.Drawing.Point(0, 27);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(397, 587);
+            this.previewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.previewBox.TabIndex = 1;
+            this.previewBox.TabStop = false;
+            this.previewBox.Click += new System.EventHandler(this.PreviewBox_Click);
+            // 
+            // menuImportExcel
+            // 
+            this.menuImportExcel.Name = "menuImportExcel";
+            this.menuImportExcel.Size = new System.Drawing.Size(180, 22);
+            this.menuImportExcel.Text = "Excelを取り込む(&I)";
+            this.menuImportExcel.Click += new System.EventHandler(this.MenuImportExcel_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // menuExit
+            // 
+            this.menuExit.Name = "menuExit";
+            this.menuExit.Size = new System.Drawing.Size(180, 22);
+            this.menuExit.Text = "終了(&X)";
+            this.menuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGrid.Location = new System.Drawing.Point(0, 32);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 21;
+            this.dataGrid.Size = new System.Drawing.Size(617, 555);
+            this.dataGrid.TabIndex = 2;
+            this.dataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGrid_CellContentClick);
+            this.dataGrid.SelectionChanged += new System.EventHandler(this.DataGrid_SelectionChanged);
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.Controls.Add(this.btnAllClear);
+            this.rightPanel.Controls.Add(this.btnAllSelect);
+            this.rightPanel.Controls.Add(this.dataGrid);
+            this.rightPanel.Location = new System.Drawing.Point(403, 27);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(617, 587);
+            this.rightPanel.TabIndex = 3;
+            // 
+            // mainStatusBar
+            // 
+            this.mainStatusBar.Location = new System.Drawing.Point(0, 617);
+            this.mainStatusBar.Name = "mainStatusBar";
+            this.mainStatusBar.Size = new System.Drawing.Size(1020, 22);
+            this.mainStatusBar.TabIndex = 4;
+            this.mainStatusBar.Text = "statusStrip1";
+            // 
+            // xlsOpenDlg
+            // 
+            this.xlsOpenDlg.DefaultExt = "xlsx";
+            this.xlsOpenDlg.Filter = "Excel|*.xlsx|古いExcel|*.xls|すべて|*.*";
+            // 
+            // btnAllSelect
+            // 
+            this.btnAllSelect.Location = new System.Drawing.Point(3, 3);
+            this.btnAllSelect.Name = "btnAllSelect";
+            this.btnAllSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnAllSelect.TabIndex = 3;
+            this.btnAllSelect.Text = "すべて選択";
+            this.btnAllSelect.UseVisualStyleBackColor = true;
+            this.btnAllSelect.Click += new System.EventHandler(this.BtnAllSelect_Click);
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Location = new System.Drawing.Point(84, 3);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(75, 23);
+            this.btnAllClear.TabIndex = 4;
+            this.btnAllClear.Text = "すべてクリア";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            this.btnAllClear.Click += new System.EventHandler(this.BtnAllClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 639);
+            this.Controls.Add(this.mainStatusBar);
+            this.Controls.Add(this.rightPanel);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "MainForm";
+            this.Text = "Ebi";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.rightPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem menuPrint;
+        private System.Windows.Forms.PictureBox previewBox;
+        private System.Windows.Forms.ToolStripMenuItem menuImportExcel;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuExit;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.StatusStrip mainStatusBar;
+        private System.Windows.Forms.OpenFileDialog xlsOpenDlg;
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Button btnAllSelect;
+    }
+}
+
---
+++ Ebi/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.IO;
+using NPOI.SS.UserModel;
+
+namespace Ebi
+{
+    public partial class MainForm : Form
+    {
+        private Image ebi = null;
+        private Image a4img = null;
+        private int printcnt = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            // エビを読み込んでおく
+            ebi = Image.FromFile("./resources/ebi.png");
+
+            calcSize();
+            createBuffer();
+            updateImage();
+            loadExcel(null);
+        }
+
+
+        private void loadExcel(String filename)
+        {
+            var dt = dataGrid.DataSource as DataTable;
+            if(dt != null)
+            {
+                dataGrid.DataSource = null;
+                dt.Dispose();
+                dt = null;
+            }
+
+            DataColumn[] cols = new DataColumn[]
+            {
+                 new DataColumn("no", typeof(int))
+                ,new DataColumn("target", typeof(bool))
+                ,new DataColumn("name1", typeof(String))
+                ,new DataColumn("name2", typeof(String))
+                ,new DataColumn("price_jp", typeof(String))
+            };
+
+            dt = new DataTable();
+            foreach(DataColumn col in cols)
+            {
+                dt.Columns.Add(col);
+            }
+
+            if(File.Exists(filename) == true)
+            {
+                // Excelを読み込んでDataTableに設定する
+                IWorkbook book = WorkbookFactory.Create(filename);
+
+                // 最初のシートが対象のシートとする
+                ISheet sheet = book.GetSheetAt(0);
+
+                IRow row = sheet.GetRow(0);
+                var printIdxPos = -1;
+                var printNamePos = -1;
+                var printPricePos = -1;
+
+                foreach (ICell cell in row.Cells)
+                {
+                    if (cell.StringCellValue == "張出" || cell.StringCellValue == "貼出")
+                    {
+                        printIdxPos = cell.ColumnIndex;
+                    }
+                    if (cell.StringCellValue == "貼出用店名")
+                    {
+                        printNamePos = cell.ColumnIndex;
+                    }
+                    if (cell.StringCellValue == "張出金額" || cell.StringCellValue == "貼出金額")
+                    {
+                        printPricePos = cell.ColumnIndex;
+                    }
+                }
+
+                var cnt = 1;
+                for (int i = 1; i < sheet.LastRowNum; i++)
+                {
+                    row = sheet.GetRow(i);
+                    var targetFlag = row.Cells[printIdxPos].StringCellValue;
+                    var name1 = row.Cells[printNamePos].StringCellValue;
+                    var name2 = row.Cells[printNamePos + 1].StringCellValue;
+                    var price = row.Cells[printPricePos].StringCellValue;
+
+                    if(name1 == "" && name2 == "")
+                    {
+                        // 名前な脚は無視
+                        continue;
+                    }
+
+                    var newRow = dt.NewRow();
+                    newRow["no"] = cnt;
+                    newRow["target"] = (targetFlag == "〇" || targetFlag == "○") && price != "" ? true : false;
+                    newRow["price_jp"] = price;
+                    newRow["name1"] = name1;
+                    newRow["name2"] = name2;
+                    dt.Rows.Add(newRow);
+                    cnt++;
+                }
+                book.Close();
+                //book.Dispose();
+            }
+
+
+            var col1 = new DataGridViewTextBoxColumn();
+            col1.DataPropertyName = "no";
+            col1.Name = "NO";
+            col1.ReadOnly = true;
+
+            var col2 = new DataGridViewCheckBoxColumn();
+            col2.DataPropertyName = "target";
+            col2.Name = "対象";
+
+            var col3 = new DataGridViewTextBoxColumn();
+            col3.DataPropertyName = "price_jp";
+            col3.Name = "金額(漢字)";
+            col3.ReadOnly = true;
+
+            var col4 = new DataGridViewTextBoxColumn();
+            col4.DataPropertyName = "name1";
+            col4.Name = "名前1";
+            col4.ReadOnly = true;
+
+            var col5 = new DataGridViewTextBoxColumn();
+            col5.DataPropertyName = "name2";
+            col5.Name = "名前2";
+            col5.ReadOnly = true;
+
+            dataGrid.Columns.Add(col1);
+            dataGrid.Columns.Add(col2);
+            dataGrid.Columns.Add(col3);
+            dataGrid.Columns.Add(col4);
+            dataGrid.Columns.Add(col5);
+
+            dataGrid.DataSource = dt;
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGrid.AutoResizeColumns();
+        }
+
+        /// <summary>
+        /// ウィンドウのサイズ変更
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            calcSize();
+        }
+
+        private void calcSize()
+        {
+            var clientHeight = this.ClientSize.Height - mainMenu.Height - 8 - mainStatusBar.Height;
+            var width = (int)(Math.Round(clientHeight * 0.707071, 0));
+            previewBox.Width = width;
+            previewBox.Height = clientHeight;
+
+            rightPanel.Width = this.ClientSize.Width - width - 8;
+            rightPanel.Height = clientHeight;
+            rightPanel.Left = width + 8;
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+
+        private void createBuffer()
+        {
+            if(a4img != null)
+            {
+                a4img.Dispose();
+                a4img = null;
+            }
+            a4img = new Bitmap(ebi.Width, ebi.Height) as Image;
+        }
+
+        private void updateImage(int targetRow = -1)
+        {
+            StringFormat sf = new StringFormat(StringFormatFlags.DirectionVertical);
+            using (Graphics graph = Graphics.FromImage(a4img))
+            using (SolidBrush brush = new SolidBrush(Color.Black))
+            using (Font priceFont = new Font("HGP行書体", 250))
+            using (Font nameFont = new Font("HGP行書体", 250))
+            {
+                graph.DrawImage(ebi, 0, 0, ebi.Width, ebi.Height);
+
+                if (dataGrid.Rows.Count > 0)
+                {
+                    var rowIdx = 0;
+                    if(targetRow == -1)
+                    {
+                        rowIdx = dataGrid.SelectedCells[0].RowIndex;
+
+                    }
+                    else
+                    {
+                        rowIdx = targetRow;
+
+                    }
+                    bool isTarget = (bool)dataGrid.Rows[rowIdx].Cells["対象"].Value;
+                    String price = (String)dataGrid.Rows[rowIdx].Cells["金額(漢字)"].Value;
+                    if(price != "")
+                    {
+                        price = "一、" + price + "圓也";
+                    }
+                    String name1 = (String)dataGrid.Rows[rowIdx].Cells["名前1"].Value;
+                    String name2 = (String)dataGrid.Rows[rowIdx].Cells["名前2"].Value;
+
+                    // 金額の描画
+                    graph.DrawString(price, priceFont, brush, 1900, 64, sf);
+
+                    if(name2 == "")
+                    {
+                        // name1の描画
+                        graph.DrawString(name1, nameFont, brush, 1000, 64 + 200, sf);
+                    }
+                    else
+                    {
+                        // name1の描画
+                        graph.DrawString(name1, nameFont, brush, 1200, 64 + 200, sf);
+
+
+                        SizeF name2Size = graph.MeasureString(name2, nameFont, new PointF(0, 0), sf);
+                        //var name2Top = a4img.Height - (name2Size.Height + 80);
+                        var name2Top = 80 + 600;
+
+                        // name2の描画
+                        graph.DrawString(name2, nameFont, brush, 600, name2Top, sf);
+
+                    }
+                }
+
+            }
+
+
+            // 描画結果を転送
+            previewBox.Image = a4img;
+            //previewBox.Update();
+        }
+
+        private void MenuPrint_Click(object sender, EventArgs e)
+        {
+            var pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(printing);
+
+            var dlg = new PrintDialog();
+
+            dlg.Document = pd;
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                printcnt = 1;
+                pd.Print();
+            }
+        }
+
+        private void printing(Object sender, PrintPageEventArgs e)
+        {
+            
+            while( printcnt < dataGrid.RowCount && ( (bool) dataGrid.Rows[printcnt].Cells["対象"].Value ) == false)
+            {
+                printcnt++;
+            } 
+            if(printcnt >= dataGrid.RowCount)
+            {
+                e.HasMorePages = false;
+            }
+            else
+            {
+                updateImage(printcnt);
+                e.Graphics.DrawImage(a4img, e.MarginBounds);
+                e.HasMorePages = true;
+
+            }
+            printcnt++;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PreviewBox_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        /// <summary>
+        /// 終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            if(a4img!= null)
+            {
+                a4img.Dispose();
+                a4img = null;
+            }
+
+            this.Close();
+        }
+
+        /// <summary>
+        /// Excelのインポート
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuImportExcel_Click(object sender, EventArgs e)
+        {
+            if(xlsOpenDlg.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            try
+            {
+                loadExcel(xlsOpenDlg.FileName);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(this, "読み込もうとしたファイルが既にExcelで開かれている、または読み込もうとしたファイルに誤りがあります。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void DataGrid_SelectionChanged(object sender, EventArgs e)
+        {
+            var selectedRowIndex = this.dataGrid.SelectedCells[0].RowIndex;
+            updateImage();
+
+        }
+
+        private void BtnAllSelect_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i <  dataGrid.RowCount; i++)
+            {
+                if((String)(dataGrid.Rows[i].Cells["名前1"].Value) != "" && (String)(dataGrid.Rows[i].Cells["名前2"].Value) != "")
+                {
+                    dataGrid.Rows[i].Cells["対象"].Value = true;
+
+                }
+            }
+        }
+
+        private void BtnAllClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGrid.RowCount; i++)
+            {
+                dataGrid.Rows[i].Cells["対象"].Value = false;
+            }
+
+        }
+    }
+}
